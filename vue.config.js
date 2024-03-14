8d2805c06f581496e14d793b074cb35843bf0eb5
Qctopus <128568287+Qctopus@users.noreply.github.com>
2024-03-14 12:12:18 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require('@vue/cli-service');
 
-module.exports = {
+module.exports = defineConfig({
+  transpileDependencies: true,
+  // Set publicPath conditionally for production and development environments
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/USPC-Map/'
-    : '/',
+    ? '/USPC-Map/' // Use this path when building for production (for GitHub Pages)
+    : '/', // Use root path for development
+  
   pluginOptions: {
     vuetify: {
       // Placeholder for Vuetify loader options
