c077c9932208223e43bdcd1b64d598904d7d88f8
Qctopus <128568287+Qctopus@users.noreply.github.com>
2024-03-14 11:30:44 +0900

    Update vue config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: './', // Makes paths relative to index.html
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
   }
-})
+});
