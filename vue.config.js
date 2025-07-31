833c703e298f9a64965d06cf3bd212de7b562296
Ahmed Ali <ahmedali040697@gmail.com>
2025-08-01 01:31:28 +0500

    Update vue.config.js for GitHub Pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './',
-  outputDir: 'dist',
-  assetsDir: 'static',
+  publicPath: process.env.NODE_ENV === "production" ? "/greening-moonshot-map/" : "/",
+  outputDir: "dist",
+  assetsDir: "static",
   productionSourceMap: false,
-  
+
   pluginOptions: {
-    vuetify: {}
-  }
+    vuetify: {},
+  },
 });
